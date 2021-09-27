b1196a9aed20bb5cde8ee0e75fb5ce38940dd779
Kat <bolivarkat@yahoo.com>
2021-09-26 19:12:49 -0500

    commit tree 14f652d5ffc72adf33ac423ccac853d37f551838

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+const formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject);
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type" : "application/json",
+      "Accept" : "application/json"
+    },
+    body: JSON.stringify({
+      name: `${name}`,
+      email : `${email}`
+    })
+  })
+  .then(resp => resp.json())
+  .then(data => {
+    document.querySelector("body").append(data.id)
+  })
+  .catch (error => {
+    alert("Something is very wrong.");
+    console.log(error.message);
+    document.querySelector("body").append(error.message)
+  })
+}
+
